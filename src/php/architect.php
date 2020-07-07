bae762edf2ab05db9d0bebc8bbe483e624fb1a96
Anders Jurisoo <jurisoo@hotmail.com>
2020-07-07 15:38:50 +0200

    Adding a dir src/php for the Architect php app

---
+++ src/php/architect.php
@@ -0,0 +1,19 @@
+<?php
+
+// Require the Architect and other dependencies
+require_once(__DIR__ . '/vendor/autoload.php');
+
+// Get target path
+$projectPath = $argv[1];
+
+// Bootstrap target
+require_once($projectPath . '/vendor/autoload.php');
+$app = require_once($projectPath . '/bootstrap/app.php');
+
+$kernel = $app->make(\Illuminate\Contracts\Console\Kernel::class);
+$kernel->bootstrap();
+
+$app->register(\Archetype\ServiceProvider::class);
+
+// Output JSON to be consumed by our electron app
+echo app('LaravelFile')::models()->get()->map->className();
