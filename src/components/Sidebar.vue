e96157f4c92f8fe7fbd67311e5da159164cf0daf
Anders Jurisoo <jurisoo@hotmail.com>
2020-07-20 10:10:41 +0200

    Stripped app from unused icons

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,12 +2,12 @@
     <div class="flex flex-col min-h-screen items-center bg-gray-700 text-gray-400 text-sm">
         <div v-if="this.$store.state.app">
             <div :class="buttonStyle()" @click="setPage('AppSummary')" ><i class="fas fa-home"></i></div>
-            <div :class="buttonStyle()" @click="refresh" ><i class="fas fa-sync-alt"></i></div>
-            <div :class="buttonStyle()" @click="setPage('AppSchema')" ><i class="fas fa-grip-vertical"></i></div>
+            <!--<div :class="buttonStyle()" @click="refresh" ><i class="fas fa-sync-alt"></i></div>-->
+            <!--<div :class="buttonStyle()" @click="setPage('AppSchema')" ><i class="fas fa-grip-vertical"></i></div>-->
             <div :class="buttonStyle()" @click="setPage('AppSchema')" ><i class="fas fa-project-diagram"></i></div>
-            <div :class="buttonStyle()" @click="create" ><i class="fas fa-plug"></i></div>
-            <div :class="buttonStyle()" @click="create" ><i class="fas fa-wave-square"></i></div>
-            <div :class="buttonStyle()" @click="create" ><i class="fas fa-check"></i></div>
+            <!--<div :class="buttonStyle()" @click="create" ><i class="fas fa-plug"></i></div>-->
+            <!--<div :class="buttonStyle()" @click="create" ><i class="fas fa-wave-square"></i></div>-->
+            <!--<div :class="buttonStyle()" @click="create" ><i class="fas fa-check"></i></div>-->
         </div>
 
         <div>
