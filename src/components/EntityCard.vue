9f2f01b2c46dd1e14a6f7dfe9bfd31d0d304b468
Anders Jurisoo <jurisoo@hotmail.com>
2020-07-28 09:48:09 +0200

    ACtive element + tabbing experiments

--- src/components/EntityCard.vue
+++ src/components/EntityCard.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="w-1/3 rounded mx-2 my-2 bg-gray-700">
+    <div tabindex="0" class="w-1/3 rounded mx-2 my-2 bg-gray-700" @keyup.enter="enterClicked()">
         <div class="flex justify-between bg-gray-600 p-2">
-            <h3 :id="entity.model" class="text-sm text-gray-200 font-bold cursor-pointer">{{ entity.model }}</h3>
+            <h3 tabindex="0" :id="entity.model" class="text-sm text-gray-200 font-bold cursor-pointer">{{ entity.model }}</h3>
         </div>
         <div class="bg-gray-700 text-gray-500 text-xs m-2">
             <ul class="">
-                <li v-for="column in entity.columns" v-bind:key="column.name">
+                <li v-for="column in entity.columns" v-bind:key="column.name"
+                    tabindex="0"
+                >
                     {{ column.name }}
                 </li>
             </ul>                    
@@ -17,10 +19,25 @@
 export default {
     props: ['entity'],
     data() {
-        return {}
+        return {
+            isFocused: false,
+        }
     },
     methods: {
-        //
-    }
+        focusChanged (event) {
+            const el = event.target
+            // do something with the element.
+            console.log(el._uid)
+        },
+        enterClicked(){
+            if(this.isFocused) alert("HEY!!");
+        }
+    },
+    created() {
+        document.addEventListener('focusin', this.focusChanged)
+    },
+    beforeDestroy() {
+        document.removeEventListener('focusin', this.focusChanged)
+    },    
 }
 </script>
--- src/components/NewEntityCard.vue
+++ src/components/NewEntityCard.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="w-1/3 mx-2 my-2 bg-gray-700 border border-orange-300 shadow shadow-lg">
+    <div tabindex="0"  class="w-1/3 mx-2 my-2 bg-gray-700">
         <div class="flex justify-between bg-gray-600 p-2">
-            <h3 class="text-sm text-gray-200 font-bold cursor-pointer">New model</h3>
+            <h3 v-if="active" tabindex="0" @keyup.enter="input()"
+                class="text-sm text-gray-200 font-bold cursor-pointer"
+                v-text="'New model'"></h3>
+            <input v-else tabindex="0" @keyup.enter="input()"
+                class="text-sm w-full bg-gray-300 text-gray-800 font-bold cursor-pointer">
         </div>
         <div class="bg-gray-700 text-gray-500 text-xs m-2">
             <ul class="">
@@ -16,10 +20,14 @@
 <script>
 export default {
     data() {
-        return {}
+        return {
+            active: false
+        }
     },
     methods: {
-        //
+        input() {
+            this.active = !this.active
+        }
     }
 }
 </script>
