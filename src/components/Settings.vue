437fb938d72d3025595a0b27effd07815fb39810
Anders Jurisoo <jurisoo@hotmail.com>
2020-08-03 17:02:25 +0200

    Stylings

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -3,18 +3,30 @@
         <div class="mx-8 my-2">
             <h1 class="text-gray-500 border-b mb-8 py-2">settings</h1>
             <form class="w-full max-w-sm">
-            <div class="md:flex md:items-center mb-6">
-                <div class="md:w-1/3">
-                <label class="block font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-                    Code home
-                </label>
-                <div class="text-xs my-2">The directory where you keep your projects.</div>
+                <div class="mb-6">
+                    <div class="">
+                        <label class="block font-bold mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Code home
+                        </label>
+                        <div class="text-xs my-2">The directory where you keep your projects.</div>
+                    </div>
+                    <div class="">
+                        <input class="bg-gray-200 text-sm appearance-none border-2 border-gray-200 rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"
+                        :value="this.$store.state.codeHome" @click="setCodeHome">
+                    </div>
                 </div>
-                <div class="md:w-2/3">
-                    <input class="bg-gray-200 text-sm appearance-none border-2 border-gray-200 rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"
-                    :value="this.$store.state.codeHome" @input="setCodeHome">
-                </div>
-            </div>
+                <div class="mb-6">
+                    <div class="">
+                        <label class="block font-bold mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Plugins directory
+                        </label>
+                        <div class="text-xs my-2">Where to load plugins from</div>
+                    </div>
+                    <div class="">
+                        <input class="bg-gray-200 text-sm appearance-none border-2 border-gray-200 rounded w-full py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text"
+                        :value="this.$store.state.pluginsDirectory" @click="setPluginsDirectory">
+                    </div>
+                </div>                
             </form>
 
         </div>
@@ -28,7 +40,12 @@ export default {
         setCodeHome() {
             const { ipcRenderer } = require('electron')
             ipcRenderer.send('set-code-home-browse')
-        }
+        },
+
+        setPluginsDirectory() {
+            const { ipcRenderer } = require('electron')
+            ipcRenderer.send('set-plugins-directory-browse')
+        }        
     }
 }
 </script>
